78cf9bb054078fca707877b37915c1e819e703f4
Jovmarti <jovmarti@ucsc.edu>
2023-07-27 10:01:23 -0700

    added text and styling to sunrun page

---
+++ src/components/Sunrun.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/Sunrun.scss";
+
+function Sunrun() {
+  return (
+    <section className="sunrun-container">
+      <div className="sunrun-flex">
+        <div>
+          <p>
+            SUNRUN <br />
+            Certified Partner
+          </p>
+        </div>
+        <div>
+          <p>
+            SUNRUN is the nation's leading residential solar, storage <br /> and
+            energy services company.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Sunrun;
