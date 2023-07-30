152dcc8643fb106a0d8542829d20f7390e560c5c
Jovmarti <jovmarti@ucsc.edu>
2023-07-30 14:02:23 -0700

    added alt text to images

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Images.scss";
-import working from "../images/working.jpg";
+// import working from "../images/working.jpg";
 import roof from "../images/roof.jpg";
 import workingMan from "../images/solar-working.jpg";
 
@@ -9,18 +9,10 @@ function Images() {
     <div className="images-container">
       <section className="images-flex">
         <section className="working">
-          <img
-            src={workingMan}
-            className="image-bottom floating floating-two"
-            alt="silver hair with colorful highlights"
-          />
+          <img src={workingMan} alt="man holding a ladder in front of panels" />
         </section>
         <section className="roof">
-          <img
-            src={roof}
-            className="image-bottom floating floating-two"
-            alt="silver hair with colorful highlights"
-          />
+          <img src={roof} alt="overview of houses with solar panels on roof" />
           <p>
             Join the community of over 800,000 customers across 23 states who
             are part of SUNRUN'S solar energy program.
