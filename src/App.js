d1a18da371d379c37f04605a973c1d6270d53f53
Jovmarti <jovmarti@ucsc.edu>
2023-07-31 12:24:06 -0700

    added text, iamges, and styling to benefits page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Home from "./components/Home.js";
 import Sunrun from "./components/Sunrun";
 import Images from "./components/Images";
+import Benefits from "./components/Benefits";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <Sunrun />
       <Images />
+      <Benefits />
     </>
   );
 }
---
+++ src/components/Benefits.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "../styles/Benefits.scss";
+import money from "../images/money.png";
+import energy from "../images/energy.png";
+import home from "../images/home.png";
+import transfer from "../images/transfer.png";
+import gear from "../images/gear.png";
+import leaf from "../images/leaf.png";
+
+function Benefits() {
+  return (
+    <section className="benefits-container">
+      <h2>Benefits Of Solar Energy</h2>
+      <div className="benefits-flex">
+        <div className="benefit-box">
+          <img src={money} alt="dollar sign" />
+          <span>Energy Independence</span>
+          <p>
+            Solar energy grants you independence by knowing your monthly rate
+            and shielding against rising energy costs.
+          </p>
+        </div>
+        <div className="benefit-box">
+          <img src={energy} alt="dollar sign" />
+          <span>Power Resilience</span>
+          <p>
+            Solar energy ensures power during grid outages, providing peace of
+            mind and continued electricity supply.
+          </p>
+        </div>
+        <div className="benefit-box">
+          <img src={home} alt="dollar sign" />
+          <span>Increased Property Value</span>
+          <p>
+            Installing solar panels can enhance the value of your property,
+            making it more attractive to potential buyers.
+          </p>
+        </div>
+      </div>
+      <div className="benefits-flex">
+        <div className="benefit-box">
+          <img src={gear} alt="dollar sign" />
+          <span>Durable & Low Maintenance</span>
+          <p>
+            Solar panels have a 25-30 year lifespan with minimal maintenance,
+            making them a reliable and cost-effective long-term investment.
+          </p>
+        </div>
+        <div className="benefit-box">
+          <img src={transfer} alt="dollar sign" />
+          <span>Easy Transferability</span>
+          <p>
+            Solar systems are easily transferable without liens or loans,
+            offering flexibility when selling or relocating.
+          </p>
+        </div>
+        <div className="benefit-box">
+          <img src={leaf} alt="dollar sign" />
+          <span>Environmental Protection</span>
+          <p>
+            By using solar energy, you actively contribute to environmental
+            protection by reducing carbon emissions & your ecological footprint.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Benefits;
