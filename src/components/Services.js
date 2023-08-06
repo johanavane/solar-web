d10f41f0c67ff366b3b3e61b41d935b8935a4804
Jovmarti <jovmarti@ucsc.edu>
2023-08-06 15:02:16 -0700

    created component for form and separated from services.js, added form validation to form and displayed validation message

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,27 +1,8 @@
 import React from "react";
-import emailjs from "emailjs-com";
+import Form from "./Form.js";
 import "../styles/Services.scss";
-import { MdEmail } from "react-icons/md";
-import { BsTelephoneFill } from "react-icons/bs";
-const API_KEY = process.env.REACT_APP_API_KEY;
-const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
 
 function Services() {
-  function sendEmail(e) {
-    // console.log("submit pressed");
-    e.preventDefault();
-
-    emailjs.sendForm(SERVICE_ID, "template_hyzmbwt", e.target, API_KEY).then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    );
-    e.target.reset();
-  }
-
   return (
     <div className="services-container">
       <section className="services-list">
@@ -63,30 +44,7 @@ function Services() {
           </li>
         </ul>
       </section>
-      <section className="contact-form">
-        <div className="get-in-touch">
-          <h3>Get in touch</h3>
-          <div>
-            <MdEmail />
-            <span>alfredo.moreno@sunrun.com</span>
-          </div>
-          <div>
-            <BsTelephoneFill />
-            <span>(408)413-8096</span>
-          </div>
-        </div>
-        <form onSubmit={sendEmail}>
-          <label>Name</label>
-          <input id="name" name="name" type="text" />
-          <label>Email</label>
-          <input id="email" name="email" type="email" />
-          <label>Message</label>
-          <textarea id="message" name="message" type="text" />
-          <button>
-            <span>Submit</span>
-          </button>
-        </form>
-      </section>
+      <Form />
     </div>
   );
 }
