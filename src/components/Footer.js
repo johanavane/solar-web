71322ff28edc5bed5adbf42ebb10acfd9e8bd5a7
Jovmarti <jovmarti@ucsc.edu>
2023-08-23 01:29:55 -0700

    added working links, made mobile ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,24 +4,34 @@ import "../styles/Footer.scss";
 function Footer() {
   return (
     <section className="footer-container">
-      <div>
-        <p className="footer-title">
-          Sunrun <br /> Certified <br /> Installer
-        </p>
+      <div className="footer-show">
+        <h3>Sunrun Certified Installer</h3>
       </div>
-      <div>
-        <p className="footer-title">Contact Me</p>
-        <p>(408)413-8096</p>
-        <p>Alfredo.moreno@sunrun.com</p>
-      </div>
-      <div>
-        <p className="footer-title">Location</p>
-        <p>Hollister, CA</p>
-        <p>Willing to travel</p>
-      </div>
-      <div>
-        <p className="footer-title">Website by</p>
-        <p>Johana M.</p>
+      <div className="footer-links">
+        <div className="footer-hide">
+          <h3 className="footer-title">
+            Sunrun <br /> Certified <br /> Installer
+          </h3>
+        </div>
+        <div>
+          <h3 className="footer-title">Contact Me</h3>
+          {/* <p>(408)413-8096</p> */}
+          <a href="tel:408-413-8096">(408)413-8096</a>
+          <a href="mailto: Alfredo.moreno@sunrun.com">
+            Alfredo.moreno@sunrun.com
+          </a>
+        </div>
+        <div>
+          <h3 className="footer-title">Location</h3>
+          <p>Hollister, CA</p>
+          <p>Willing to travel</p>
+        </div>
+        <div>
+          <a className="web-design" href="https://www.johanamartinez.com/">
+            <h3>Website by</h3>
+            <p>Johana M.</p>
+          </a>
+        </div>
       </div>
     </section>
   );
