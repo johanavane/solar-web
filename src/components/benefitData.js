5327da97f43b642b28a50cf1c1f5888221ce60c6
Jovmarti <jovmarti@ucsc.edu>
2023-09-11 21:42:36 -0700

    cleaned code, added title to website

---
+++ src/components/benefitData.js
@@ -0,0 +1,53 @@
+import money from "../images/money.png";
+import energy from "../images/energy.png";
+import home from "../images/home.png";
+import transfer from "../images/transfer.png";
+import gear from "../images/gear.png";
+import leaf from "../images/leaf.png";
+
+const benefitData = [
+  {
+    title: "Energy Independence",
+    description:
+      "Solar energy grants you independence by knowing your monthly rate and shielding against rising energy costs.",
+    src: money,
+    alt: "dollar icon",
+  },
+  {
+    title: "Power Resilience",
+    description:
+      "Solar energy ensures power during grid outages, providing peace of mind and continued electricity supply.",
+    src: energy,
+    alt: "lightning icon",
+  },
+  {
+    title: "Increased Property Value",
+    description:
+      "Installing solar panels can enhance the value of your property, making it more attractive to potential buyers.",
+    src: home,
+    alt: "home icon",
+  },
+  {
+    title: "Easy Transferability",
+    description:
+      "Solar systems are easily transferable without liens or loans, offering flexibility when selling or relocating.",
+    src: transfer,
+    alt: "arrow icon",
+  },
+  {
+    title: "Durable & Low Maintenance",
+    description:
+      "Solar panels have a 25-30 year lifespan with minimal maintenance, making them a reliable and cost-effective long-term investment.",
+    src: gear,
+    alt: "gear icon",
+  },
+  {
+    title: "Environmental Protection",
+    description:
+      "Using solar energy contributes to environmental protection by reducing carbon emissions & your ecological footprint.",
+    src: leaf,
+    alt: "leaf icon",
+  },
+];
+
+export default benefitData;
